4c9946d00a19c2455899456d5bb0d218f5e549e5
Daniel Emery <danielremery@gmail.com>
2024-07-06 11:08:18 +0200

    #99 Fix sentry v8 breaking changes

---
+++ src/instrument.ts
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/node';
+import config from './config/config';
+Sentry.init({
+  dsn: config.SENTRY_DSN,
+  integrations: [Sentry.prismaIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+  environment: config.DOPPLER_CONFIG,
+  release: config.QUIZLORD_VERSION,
+});
