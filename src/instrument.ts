7eb258bb7f4388c9720fd2d8ecaf4fde8a2b5389
Daniel Emery <danielremery@gmail.com>
2025-03-21 08:45:58 +0100

    Add sentry node profiling integration

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 import config from './config/config';
 
+console.log('Initialising Sentry instrumentation');
 Sentry.init({
   dsn: config.SENTRY_DSN,
-  integrations: [Sentry.prismaIntegration()],
+  integrations: [Sentry.prismaIntegration(), nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
   environment: config.DOPPLER_CONFIG,
